7f0947c77a2971c3b00985f1b4c211c27d88a57c
James Wordie <james.wordie@yahoo.com>
2021-06-04 11:50:16 +0100

    changes to the models

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,12 @@
 # Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 # Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-require "json"
 require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
+Movie.destroy_all
+puts "Database cleaned"
 
 url = "http://tmdb.lewagon.com/movie/top_rated"
 parsed = JSON.parse(URI.open(url).read)
